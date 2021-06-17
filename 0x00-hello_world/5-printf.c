3285c3e7224cedb98843b434cad007874ceae5bb
olaredenish2017 <olareodhiambo@yahoo.com>
2021-06-17 04:22:04 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -15,7 +15,7 @@
 
 int main(void)
 {
-             printif("with proper grammer, but the outcome is a piece of art,\n");
+             printf("with proper grammer, but the outcome is a piece of art,\n");
 
                    return (0);
 
