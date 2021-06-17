afb219e418f2aa95fd931c85bdb8d76e1a72cea9
olaredenish2017 <olareodhiambo@yahoo.com>
2021-06-17 03:02:10 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#!/bin/bash
+   /*
+    *File: 4-puts.c
+    *Auth: Olare odhiambo
+    */
+
+#include <stdio.h>
+
+   /**
+    *main - Prints "Programming is building a multilingual puzzle, followed by a new line.
+    *
+    *Return: Always 0
+    */
+init main(void)
+
+{
+  puts("\"Programming is like building a multilingual puzzle");
+
+  return (0);
+  
+    }
