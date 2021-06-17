d0adb0c209b151fe3a61bb3b574d0b90a5410128
olaredenish2017 <olareodhiambo@yahoo.com>
2021-06-17 04:14:25 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,22 @@
+/*
+ *File: 5-printf.c
+ *Auth: Olare odhiambo
+ *
+ */
+
+#include <stdio.h>
+
+/**
+ *main - Prints "with proper grammer, but the outcome is
+ *                                                       a piece of art," followed by a new line.
+ *
+ *Return: Always 0.
+ */
+
+int main(void)
+{
+             printif("with proper grammer, but the outcome is a piece of art,\n");
+
+                   return (0);
+
+}
